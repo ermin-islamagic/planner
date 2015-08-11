18ae53b418bbe8d092ae71102e79e6eb3522c740
hamleet <ermin@islamagic.com>
2015-08-11 09:47:23 +0200

    updated

--- application/views/pages/people/read.php
+++ application/views/pages/people/read.php
@@ -1,10 +1,11 @@
+<br>
 <section>
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
 
                 <div class="table-responsive">
-                    <table class="table">
+                    <table class="table table-bordered">
                         <thead>
                         <tr>
                             <th>ID</th>
@@ -18,11 +19,68 @@
                             <td>Ermin Ivory</td>
                             <td>Web Developer</td>
                         </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>Ermin Ivory</td>
+                            <td>Web Developer</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>Ermin Ivory</td>
+                            <td>Web Developer</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>Ermin Ivory</td>
+                            <td>Web Developer</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>Ermin Ivory</td>
+                            <td>Web Developer</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>Ermin Ivory</td>
+                            <td>Web Developer</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>Ermin Ivory</td>
+                            <td>Web Developer</td>
+                        </tr>
                         </tbody>
                     </table>
                 </div>
 
             </div>
         </div>
+
+        <div class="row">
+            <div class="col-lg-12">
+
+                <nav>
+                    <ul class="pagination">
+                        <li>
+                            <a href="#" aria-label="Previous">
+                                <span aria-hidden="true">&laquo;</span>
+                            </a>
+                        </li>
+                        <li><a href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                        <li><a href="#">4</a></li>
+                        <li><a href="#">5</a></li>
+                        <li>
+                            <a href="#" aria-label="Next">
+                                <span aria-hidden="true">&raquo;</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+
+            </div>
+        </div>
+
     </div>
 </section>
